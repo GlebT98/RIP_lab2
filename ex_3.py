1641af749fb7979cb0335bcab15481e0ca541094
GlebT98 <43340447+GlebT98@users.noreply.github.com>
2019-10-25 21:17:15 +0300

    Add files via upload

---
+++ ex_3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+data = [4, -30, 100, -100, 123, 1, 0, -1, -4]
+# Реализация задания 3
+import math
+
+# Самый короткий вариант
+print(sorted(data, key=abs))
+# Более длинный вариант
+# print(sorted([data[x] for x in range(len(data))], key=abs))
+# Выводит первыми отрицательные числа
+#print(sorted([data[x] for x in range(len(data))], key=lambda n: (abs(-n), n)))
---
+++ ex_5.py
@@ -0,0 +1,19 @@
+from time import sleep
+from librip.ctxmngrs import timer
+
+with timer():
+    sleep(5.5)
+
+    from time import sleep
+from librip.ctxmngrs import timer
+
+
+# with timer():
+#    sleep(5.5)
+
+@timer
+def	good():
+	import time
+	time.sleep(5.5)
+
+good()
